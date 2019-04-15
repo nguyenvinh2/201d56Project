7ad0491fdf9d09460d399ef6f3845544e13039f5
Matt Burger <matt.burger1@outlook.com>
2019-04-15 12:43:27 -0700

    change code style to codefellows style

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ var allCards = [];
 var firstNameArr = ['charlie','cristian','david','demi','devon','jhia','john','jorie','kent','lena','levi','liz','matt','osunkwo','paula','peter','robert','roger','sam','stephen','tim','vinh','yuan','paolo'];
 var lastNameArr = ['winters','restrepo','marchante','hamm','hackley','turner','winters','fernandez','ketter','eivy','porter','mahoney','guesswho','williams','thomas','tynan','bronson','huba','hamm','chu','busch','nguyen','gao','chidrome'];
 
-function Card(firstName,lastName,value)
-{
+function Card(firstName,lastName,value){
   this.filePath = '/assets/facePics/' + firstName + '.jpeg';
   this.firstName = firstName;
   this.lastName = lastName;
@@ -15,8 +14,7 @@ function Card(firstName,lastName,value)
   allCards.push(this);
 }
 
-for(let i = 0; i < firstNameArr.length; i++)
-{
+for(let i = 0; i < firstNameArr.length; i++){
   new Card(firstNameArr[i],lastNameArr[i],i);
 }
 
