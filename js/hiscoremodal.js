90c3b8852d78171bf8e33926de31aed7287d95b5
STEPHEN CHU <stephen.chu530@gmail.com>
2019-04-17 12:42:01 -0700

    feat: added basic hi score popup functionality

---
+++ js/hiscoremodal.js
@@ -0,0 +1,41 @@
+// The following modal js is from:
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+
+// Get the modal
+var hsModal = document.getElementById('hiscoremodal');
+
+// Get the button that opens the modal
+var hsBtn = document.getElementById('hiscorebtn');
+
+// Get the <span> element that closes the modal
+var hsSpan = document.getElementById('hiscore-modal-close');
+
+// When the user clicks the button, open the modal
+hsBtn.onclick = function() {
+  renderHiScores();
+  hsModal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+hsSpan.onclick = function() {
+  hsModal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == hsModal) {
+    hsModal.style.display = 'none';
+  }
+}
+
+var renderHiScores = function() {
+  var scoresListEl = document.getElementById('hi-scores-list');
+  var scores = [];
+  scores.forEach(function(score) {
+    var newLI = document.createElement('li');
+    newLI.textContent = `${score[0]} ${score[1]}`;
+    scoreListEL.appendChild(newLI);
+  });
+};
+
+var hiScores = new Score();
